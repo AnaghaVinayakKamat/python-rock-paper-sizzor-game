4358a895bce917ac8ba3cc55941e819876813350
AnaghaVinayakKamat <84722023+AnaghaVinayakKamat@users.noreply.github.com>
2021-05-25 21:28:46 +0530

    Add files via upload

--- RockPaperSizzors.py
+++ RockPaperSizzors.py
@@ -1,7 +1,7 @@
 #import modules
 
 from tkinter import *
-import random
+from random import *
 
 #initialize the window
 root = Tk()                                            
@@ -11,47 +11,56 @@ root.title('Rock,Paper,Sizzors Game')
 root.config(bg ='black')
 Label(root, text = 'Rock, Paper ,Sizzors' , font='arial 20 bold', bg = 'white').place(x=100,y=0)
 
-#user choice heading
-user_pick= StringVar()
-Label(root, text = 'choose any one: rock, paper ,sizzor' , font='arial 15 bold', anchor= 'center', fg = 'yellow', bg='black').place(x = 65,y=40)
+#computer1 choice
 
-#computer choice
-comp_pick = random.randint(1,3)
+comp1_pick= choice(['rock', 'paper', 'sizzor'])
 
-if comp_pick == 1:
-    comp_pick = 'rock'
-elif comp_pick ==2:
-    comp_pick = 'paper'
-else:
-    comp_pick = 'sizzor'
+# computer2 choice
+
+comp2_pick= choice(['rock', 'paper', 'sizzor'])
 
 #function def
-
 def play():
+    if(comp1_pick =='rock' and comp2_pick =='paper'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print('Computer 2 wins')
+    elif(comp1_pick =='rock' and comp2_pick =='sizzor'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print('Computer 1 wins')
+    elif(comp1_pick =='paper' and comp2_pick =='rock'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print('Computer 1 wins')
+    elif(comp1_pick =='paper' and comp2_pick =='sizzor'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print('Computer 2 wins')
+    elif(comp1_pick =='sizzor' and comp2_pick =='rock'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print('Computer 2 wins')
+    elif(comp1_pick =='sizzor' and comp2_pick =='paper'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print('Computer 1 wins')
+    elif(comp1_pick =='rock' and comp2_pick =='rock'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print("It's a tie")
+    elif(comp1_pick =='paper' and comp2_pick =='paper'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print("It's a tie")
+    elif(comp1_pick =='sizzor' and comp2_pick =='sizzor'):
+        print('Computer 1 chose:',comp1_pick)
+        print('Computer 2 chose:',comp2_pick)
+        print("It's a tie")
 
-    if (user_pick == comp_pick):
-        print('tie,you both select same')
-        
-    elif (user_pick == 'rock' and comp_pick == 'paper'):
-        print('you loose,computer select paper')
-        
-    elif (user_pick == 'rock' and comp_pick == 'sizzor'):
-        print('you win,computer select sizzor')
-        
-    elif (user_pick == 'paper' and comp_pick == 'sizzor'):
-        print('you loose,computer select sizzor')
-        
-    elif (user_pick == 'paper' and comp_pick == 'rock'):
-        print('you win,computer select rock')
-        
-    elif (user_pick == 'sizzor' and comp_pick == 'rock'):
-        print('you loose,computer select rock')
+#configure buttons
+Label(root, text = 'Check the button to generate the game' , font='arial 15 bold', anchor= 'center', fg = 'yellow', bg='black').place(x = 65,y=40)
 
-    else:
-        print('you win ,computer select paper')
-        
-#Selection buttons
+game=Button(root, text='Start The Game', font='arial 30 bold', bg='black', fg='orange', command=play).place(x=90 , y=120)
 
-Button(root, text='rock', font='arial 30 bold', command=play, bg='black', fg='orange').place(x=40 , y=90)
-Button(root, text='paper', font='arial 30 bold', command=play, bg='black', fg='orange').place(x=170 , y=90)
-Button(root, text='sizzor', font='arial 30 bold', command=play, bg='black', fg='orange').place(x=320 , y=90)
+root.mainloop()
